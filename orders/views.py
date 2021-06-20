f22022382fc55afa848a15eff1d6c067eb7e8763
alv2017 <v.alishauskaite@gmail.com>
2021-06-21 00:45:25 +0300

    orders app: order list view and order detail view created.

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Order
+from .serializers import OrderSerializer, OrderDetailSerializer
+from .permissions import HasGroupPermission
 
-# Create your views here.
+
+required_groups = {
+        'GET': ('store_administrators',),
+        'HEAD': ('store_administrators',),
+        'OPTIONS': ('store_administrators',),
+    }
+
+
+class OrderListView(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = (HasGroupPermission,)
+    name = 'orders'
+
+
+class OrderDetailView(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderDetailSerializer
+    name = 'order-details'
+
+    permission_classes = (HasGroupPermission, )
+    required_groups = required_groups
+
+
+
+
+
