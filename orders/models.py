797d648918ccc73ee3295feded933e4b6a3a4124
alv2017 <v.alishauskaite@gmail.com>
2021-06-20 22:39:16 +0300

    orders app: models.py modified: model functions turned to functional properties.

--- orders/models.py
+++ orders/models.py
@@ -19,7 +19,8 @@ class Order(models.Model):
     def __str__(self):
         return f'Order {self.id}'
 
-    def get_total_cost(self):
+    @property
+    def total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
 
 
@@ -31,5 +32,6 @@ class OrderItem(models.Model):
     def __str__(self):
         return str(self.id)
 
-    def get_cost(self):
+    @property
+    def cost(self):
         return self.price * self.quantity
