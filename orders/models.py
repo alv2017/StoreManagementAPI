17c6f68c40ac9cb88204ef764dfc2fa013493c08
alv2017 <v.alishauskaite@gmail.com>
2021-06-21 10:56:53 +0300

    orders app: order models have been modified.

--- orders/models.py
+++ orders/models.py
@@ -21,13 +21,15 @@ class Order(models.Model):
 
     @property
     def total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        if self.items.count() > 0:
+            return sum(item.cost for item in self.items.all())
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
 
     def __str__(self):
         return str(self.id)
