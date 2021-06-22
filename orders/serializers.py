af00e28894cb987995a330ab44c2a85591fe1e89
alv2017 <v.alishauskaite@gmail.com>
2021-06-22 11:34:35 +0300

    orders app: Serializers have been updated.

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
-from .models import Order, OrderItem
+from .models import Order, OrderItem, OrderStatus
 from products.serializers import ProductDataSerializer
+
+
+class OrderStatusField(serializers.CharField):
+    def to_representation(self, value):
+        return value.get_status_display()
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -25,8 +30,28 @@ class OrderSerializer(serializers.ModelSerializer):
                   'created', 'updated')
 
 
+class OrderStatusSerializer(serializers.Serializer):
+    order_id = serializers.IntegerField()
+    status = serializers.CharField(max_length=1)
+
+    def create(self, validated_data):
+        return OrderStatus.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.status = validated_data.get('status', instance.status)
+        instance.save()
+        return instance
+
+
+class OrderStatusDisplaySerializer(serializers.Serializer):
+    status_name = serializers.CharField()
+    create_timestamp = serializers.DateTimeField(read_only=True)
+
+
 class OrderDetailSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+    recent_status = OrderStatusField(source='get_recent_status', read_only=True)
+    status_updates = OrderStatusDisplaySerializer(source='get_status_updates', many=True)
 
     class Meta:
         model = Order
@@ -36,6 +61,11 @@ class OrderDetailSerializer(serializers.ModelSerializer):
                   'address', 'postal_code', 'city', 'country',
                   'created', 'updated',
                   'items',
-                  'total_cost')
+                  'total_cost',
+                  'recent_status',
+                  'status_updates'
+                  )
+
+
 
 
