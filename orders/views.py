c740f60a45a9ebb9f5e45801faaa5ed1e10073f9
alv2017 <v.alishauskaite@gmail.com>
2021-06-22 12:57:48 +0300

    orders app: added views for order status follow up

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
-from .models import Order
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Order, OrderStatus
 from .serializers import OrderSerializer, OrderDetailSerializer
+from .serializers import OrderStatusSerializer, OrderStatusDisplaySerializer
 from .permissions import HasGroupPermission
 
 
@@ -28,3 +32,63 @@ class OrderDetailView(generics.RetrieveAPIView):
 
     permission_classes = (HasGroupPermission, )
     required_groups = required_groups
+
+
+class OrderStatusListCreateView(APIView):
+    name = 'order-status-updates'
+    permission_classes = (HasGroupPermission, )
+    required_groups = required_groups
+
+    def get(self, request, order_id):
+        try:
+            Order.objects.get(pk=order_id)
+        except Order.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            order_status_objects = OrderStatus.objects.filter(order_id=order_id)
+            if len(order_status_objects) == 0:
+                return Response("No status updates.")
+
+            order_status_serializer = OrderStatusSerializer(order_status_objects, many=True)
+            return Response(order_status_serializer.data)
+
+    def post(self, request, order_id):
+        try:
+            Order.objects.get(pk=order_id)
+        except Order.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            # if order is closed its status can not be updated
+            if OrderStatus.objects.filter(order_id=order_id, status=OrderStatus.CLOSED):
+                return Response("Closed order can not obtain a new status",
+                                status=status.HTTP_403_FORBIDDEN)
+            # if order is cancelled its status can not be updatade
+            if OrderStatus.objects.filter(order_id=order_id, status=OrderStatus.CANCELLED):
+                return Response("Cancelled order can not obtain a new status",
+                                status=status.HTTP_403_FORBIDDEN)
+
+            if 'status' not in request.data:
+                return Response("Bad request data", status=status.HTTP_400_BAD_REQUEST)
+
+            if 'comment' not in request.data:
+                comment = ''
+            else:
+                comment = request.data["comment"]
+
+            data = {
+                "order_id": order_id,
+                "status": request.data["status"],
+                "comment": comment,
+            }
+
+            print(data)
+
+            order_status_serializer = OrderStatusSerializer(data=data)
+            if order_status_serializer.is_valid():
+                print(data)
+                order_status_serializer.save()
+                return Response(order_status_serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(order_status_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
