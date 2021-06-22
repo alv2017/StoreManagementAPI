ce792078bcb3dfeda83b271ca05e6d1e17a14441
alv2017 <v.alishauskaite@gmail.com>
2021-06-22 12:59:22 +0300

    orders app: url have been added for order status follow up view.

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import OrderListView, OrderDetailView
+from .views import OrderStatusListCreateView
 
 urlpatterns = [
     path('', OrderListView.as_view(), name=OrderListView.name),
     path('<int:pk>/', OrderDetailView.as_view(), name=OrderDetailView.name),
+    path('status/<int:order_id>/', OrderStatusListCreateView.as_view(), name=OrderStatusListCreateView.name),
 ]
